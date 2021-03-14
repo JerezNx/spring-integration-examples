2c412a5f047e1c60d86a5f02afab3086a073e0c4
JerezNx <772691544@qq.com>
2021-03-14 19:05:40 +0800

    增加spring-actuator集成样例

---
+++ spring-monitor/spring-actuator/src/main/java/xyz/jerez/spring/monitor/actuator/SpringActuatorApplication.java
@@ -0,0 +1,17 @@
+package xyz.jerez.spring.monitor.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liqilin
+ * @since 2021/1/29 17:02
+ */
+@SpringBootApplication
+public class SpringActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringActuatorApplication.class, args);
+    }
+
+}
